ce58e017498d2372e1a5f4d3a31d3b689b6d7787
JehiraXavier <jehiraxavier@gmail.com>
2023-06-27 04:13:17 +0530

    Code cleanup

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 function NavBar() {
-    var x = document.getElementById("top_nav");
-    if (x.className === "topnav") {
+  var x = document.getElementById("top_nav");
+  if (x.className === "topnav") {
     x.className += " responsive";
-    } else {
+  } else {
     x.className = "topnav";
-    }
-    }
-    window.onscroll = function() {scroll_function()};
-    
-    function scroll_function() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  }
+}
+window.onscroll = function () {
+  scroll_function();
+};
+
+function scroll_function() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.top = "0";
     document.getElementById("roll_back").style.display = "flex";
-    } else {
+  } else {
     document.getElementById("navbar").style.top = "-100px";
     document.getElementById("roll_back").style.display = "none";
-    }
-    }
+  }
+}
